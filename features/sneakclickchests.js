74a629ccc216713e489b00f832094f204d8743f9
Jahan2cool <56746603+Jahan2cool@users.noreply.github.com>
2023-11-05 21:52:21 -0800

    Update sneakclickchests.js

--- features/sneakclickchests.js
+++ features/sneakclickchests.js
@@ -1,3 +1,12 @@
-register("GuiOpened", (event) => {
-    ChatLib.chat('yo')
+var reset = new Boolean(true);
+
+register("tick", () => {
+    if (Player.getContainer().getName() == 'container') {
+        reset = true
+    }
+    else if (Player.getContainer().getName() == 'Chest' && reset === true || Player.getContainer().getName() == 'Large Chest' && reset === true){
+        ChatLib.chat('&4[&cLearn Dungeons&4] &aRemember to &bsneak&a click chests!')
+        reset = false
+    }
+    
 })
